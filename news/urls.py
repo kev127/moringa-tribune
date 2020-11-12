8917321a2967b0702f7120cc66a12f9dc5d4289d
kev127 <kelvinkeya125@gmail.com>
2020-11-12 16:36:49 +0300

    moringa tribune

---
+++ news/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('today/',views.news_of_day,name = 'newsToday'),
+    path('archives(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews'),
+    path('search/', views.search_results, name='search_results')
+]
