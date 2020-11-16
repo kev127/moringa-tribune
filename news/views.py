d6f5fdb30a4e0705bd3e48339bb083a44311de34
kev127 <kelvinkeya125@gmail.com>
2020-11-16 13:50:17 +0300

    add newsletter and news form,newsform view functions

--- news/views.py
+++ news/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse 
 import datetime as dt
-from django.http  import HttpResponse,Http404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 from django.shortcuts import render,redirect
 from .models import Article
+from .forms import NewsLetterForm
+from .email import send_welcome_email
+
 
 
 # Create your views here.
@@ -44,7 +47,20 @@ def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
     print("new",news)
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+     if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+
+            HttpResponseRedirect('news_today')
+    else:
+        form = NewsLetterForm()
+   return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 def search_results(request):
 
--- tribune/settings.py
+++ tribune/settings.py
@@ -51,6 +51,13 @@ DATABASES['default'].update(db_from_env)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 INSTALLED_APPS = [
     'bootstrap3',
     'news.apps.NewsConfig',
